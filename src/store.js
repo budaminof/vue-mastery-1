52ffc712d38aa278b2e4e9c5574ce6a20b3defc7
Bud Aminof <budaminof@gmail.com>
2019-03-16 13:07:24 -0600

    vuex: added mapState and mapGetters

--- src/store.js
+++ src/store.js
@@ -5,12 +5,97 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    user: {
+      id: '123vbn', name: 'Bud Aminof',
+    },
+    categories: ['sustainability', 'nature', 'animal welfare',
+      'housing', 'education', 'food', 'community'],
+    events: [
+      {
+        id: 5928101,
+        user: {
+          id: 'abc123',
+          name: 'Adam',
+        },
+        category: 'animal welfare',
+        organizer: 'Adam',
+        title: 'Cat Cabaret',
+        description: 'Yay felines!',
+        location: 'Meow Town',
+        date: '2019-01-03T21:54:00.000Z',
+        time: '2:00',
+        attendees: [
+          {
+            name: 'Jonathan',
+          },
+          {
+            name: 'Bud',
+          },
+          {
+            name: 'Ryan',
+          },
+        ],
+      },
+      {
+        id: 8419988,
+        user: {
+          id: 'abc123',
+          name: 'Adam',
+        },
+        category: 'animal welfare',
+        organizer: 'Adam',
+        title: 'Kitty Cluster',
+        description: 'Yay cats!',
+        location: 'Catlandia',
+        date: '2019-01-31T22:09:00.000Z',
+        time: '7:00',
+        attendees: [
+          {
+            name: 'Jonathan',
+          },
+          {
+            name: 'Bud',
+          },
+          {
+            name: 'Ryan',
+          },
+          {
+            name: 'Erin',
+          },
+        ],
+      },
+      {
+        id: 4582797,
+        user: {
+          id: 'abc123',
+          name: 'Adam',
+        },
+        category: 'animal welfare',
+        organizer: 'Adam',
+        title: 'Puppy Parade',
+        description: 'Yay pups!',
+        location: 'Puptown ',
+        date: '2019 - 02 - 02T23: 27: 00.000Z',
+        time: '1:00',
+        attendees: [
+          {
+            name: 'Bud',
+          },
+          {
+            name: 'Erin',
+          },
+        ],
+      },
+    ],
   },
   mutations: {
 
   },
   actions: {
 
+  },
+  getters: {
+    catLength: state => state.categories.length,
+    getEventById: state => id => state.events.find(event => event.id === id),
   },
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Event Create</h1>
+  <div>
+    <h2>Create and Event, {{user.name}}</h2>
+    <p>Mapped getters {{catLength}} categories</p>
+    <p>{{getEventById(5928101)}}</p>
+    <ul>
+      <li v-for="(category, i) in categories" :key="i">{{category}}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['catLength', 'getEventById']),
+    ...mapState(['user', 'categories']),
+  },
+};
+</script>
+
+<style scoped>
+</style>
